ffc5fc5f59baddd7e9fbd9b0e76759f654ec04b0
unknown <kozhon.amantay@gmail.com>
2022-12-23 09:49:32 +0600

    feat: add filtration markup

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,13 +1,61 @@
-import { Box } from '@mui/material';
+import { Box, Button, Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material';
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
+import { purple } from '@mui/material/colors';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText('rgba(126, 91, 194, 1)'),
+  backgroundColor: 'rgba(126, 91, 194, 1)',
+  '&:hover': {
+    backgroundColor: 'rgba(100, 80, 140, 1)',
+  },
+}));
 
 const Dashboard = () => {
   return (
     <Box>
       <Header />
-      <Box></Box>
+      <Box sx={{ display: 'flex' }}>
+        <Box>
+          <Typography>Фильтрация</Typography>
+          <FormGroup>
+            <Box>
+              <FormControlLabel
+                control={<Checkbox color="primary" value="checkedA" />}
+                label="Спорт"
+              ></FormControlLabel>
+            </Box>
+            <Box>
+              <FormControlLabel
+                control={<Checkbox color="primary" value="checkedA" />}
+                label="Политика"
+              ></FormControlLabel>
+            </Box>
+            <Box>
+              <FormControlLabel
+                control={<Checkbox color="primary" value="checkedA" />}
+                label="Звезды"
+              ></FormControlLabel>
+            </Box>
+            <Box>
+              <FormControlLabel
+                control={<Checkbox color="primary" value="checkedA" />}
+                label="Искусство"
+              ></FormControlLabel>
+            </Box>
+            <Box>
+              <FormControlLabel
+                control={<Checkbox color="primary" value="checkedA" />}
+                label="Мода"
+              ></FormControlLabel>
+            </Box>
+            <ColorButton variant="contained">Применить</ColorButton>
+          </FormGroup>
+        </Box>
+        <Box></Box>
+      </Box>
       <Footer />
     </Box>
   );
